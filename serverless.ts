41adb3a47eb30861166eecd074d4b6bfd2a2a87d
Emin Deniz <44745432+emindeniz99@users.noreply.github.com>
2020-08-27 23:42:59 +0300

    initial

--- serverless.ts
+++ serverless.ts
@@ -1,44 +1,46 @@
-import type { Serverless } from 'serverless/aws';
+import type { Serverless } from "serverless/aws"
 
 const serverlessConfiguration: Serverless = {
-  service: {
-    name: 'meal-scrap-boun-node-ts',
-    // app and org for use with dashboard.serverless.com
-    // app: your-app-name,
-    // org: your-org-name,
-  },
-  frameworkVersion: '>=1.72.0',
-  custom: {
-    webpack: {
-      webpackConfig: './webpack.config.js',
-      includeModules: true
-    }
-  },
-  // Add the serverless-webpack plugin
-  plugins: ['serverless-webpack'],
-  provider: {
-    name: 'aws',
-    runtime: 'nodejs12.x',
-    apiGateway: {
-      minimumCompressionSize: 1024,
-    },
-    environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-    },
-  },
-  functions: {
-    hello: {
-      handler: 'handler.hello',
-      events: [
-        {
-          http: {
-            method: 'get',
-            path: 'hello',
-          }
-        }
-      ]
-    }
-  }
+	// app and org for use with dashboard.serverless.com
+	org: "emindeniz99",
+	app: "meal-scrap-boun-node-ts",
+	service: {
+		name: "meal-scrap-boun-node-ts",
+	},
+	frameworkVersion: ">=1.72.0",
+	custom: {
+		webpack: {
+			webpackConfig: "./webpack.config.js",
+			includeModules: true,
+		},
+	},
+	// Add the serverless-webpack plugin
+	plugins: ["serverless-webpack", "serverless-offline"],
+	provider: {
+		name: "aws",
+		runtime: "nodejs12.x",
+		region: "eu-central-1",
+		stage: "dev",
+		apiGateway: {
+			minimumCompressionSize: 1024,
+		},
+		environment: {
+			AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+		},
+	},
+	functions: {
+		hello: {
+			handler: "handler.hello",
+			events: [
+				{
+					http: {
+						method: "get",
+						path: "/",
+					},
+				},
+			],
+		},
+	},
 }
 
-module.exports = serverlessConfiguration;
+module.exports = serverlessConfiguration
